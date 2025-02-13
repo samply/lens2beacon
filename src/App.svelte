6855770073d5703c20ba1b0df13540896d0a1457
DavidCroftDKFZ <46788708+DavidCroftDKFZ@users.noreply.github.com>
2025-02-13 10:45:45 +0100

    Initial changes to page
    * Swapped EUCAIM logos with GDI logos
    * Removed unused images
    * Removed code for mobile usage
    * Modified header for GDI

--- src/App.svelte
+++ src/App.svelte
@@ -23,56 +23,7 @@
 		optionsJSON: string;
 	}> = fetchData(catalogueUrl, optionsFilePath);
 
-	//let catalogueDataPromise = getStaticCatalogue('catalogues/catalogue-eucaim.json');
-
 	let dataPasser: LensDataPasser;
-
-	/**
-	 * The following functions are the API to the library stores (state)
-	 * here you get information to use in your application
-	 * or manipulate the stores
-	 * use if needed and import types from @samply/lens
-	 */
-
-	// const getQuery = (): void => {
-	// 	console.log('getQuery()', dataPasser.getQueryAPI());
-	// };
-
-	// const getResponse = (): void => {
-	// 	console.log('getResponse()', dataPasser.getResponseAPI());
-	// };
-
-	// const getAST = (): void => {
-	// 	console.log('getAst()', dataPasser.getAstAPI());
-	// };
-
-	// const removeItem = (queryObject: QueryItem): void => {
-	// 	console.log('removeItem()', queryObject);
-	// 	dataPasser.removeItemFromQuyeryAPI({ queryObject });
-	// 	getQuery();
-	// };
-
-	// const removeValue = (queryItem: QueryItem, value: QueryValue): void => {
-	// 	console.log('removeValue()', queryItem, value);
-	// 	dataPasser.removeValueFromQueryAPI({ queryItem, value });
-	// 	getQuery();
-	// };
-	let mobileNavOpen = false;
-	const toggleMobileNav = () => {
-		mobileNavOpen = !mobileNavOpen;
-	};
-
-	window.addEventListener('resize', () => {
-		if (window.innerWidth <= 768) {
-			mobileNavOpen = false;
-			catalogueCollapsable = true;
-		} else if (window.innerWidth > 768 && window.innerWidth < 1024) {
-			mobileNavOpen = true;
-			catalogueCollapsable = true;
-		} else if (window.innerWidth >= 1024) {
-			catalogueCollapsable = false;
-		}
-	});
 
 	if (window.innerWidth >= 1024) {
 		catalogueCollapsable = false;
@@ -90,33 +41,10 @@
 </script>
 
 <header>
-	<div>
-		<a href="https://dashboard.eucaim.cancerimage.eu/">
-			<img src="../assets/images/logoEUCAIM_nav@1.5x-8.png" alt="" />
-		</a>
+	<div class="header-panel">
+		<img class="header-panel-image" src="../assets/images/Beacon.PNG" alt="Beacon" />
+		<p class="header-panel-text">Beacon search</p>
 	</div>
-	<button class="burger-menu-button" on:click="{toggleMobileNav}">
-		<div></div>
-		<div></div>
-		<div></div>
-	</button>
-	{#if mobileNavOpen}
-		<div>
-			<nav>
-				<ul>
-					<li>
-						<a href="https://dashboard.eucaim.cancerimage.eu/">HOME</a>
-					</li>
-					<li>
-						<a href="https://catalogue.eucaim.cancerimage.eu/">PUBLIC CATALOGUE</a>
-					</li>
-					<li>
-						<a href="https://help.cancerimage.eu/#login">HELPDESK</a>
-					</li>
-				</ul>
-			</nav>
-		</div>
-	{/if}
 </header>
 
 <main>
@@ -169,25 +97,11 @@
 				href="https://github.com/samply/">Samply tools</a
 			>
 			(<a href="https://github.com/samply/lens/">Lens</a>,
-			<a href="https://github.com/samply/beam/">Beam</a>,
-			<a href="https://github.com/samply/focus/">Focus</a>,
-			<a href="https://github.com/samply/bridgehead/">Bridgehead</a>), created by the
+			<a href="https://github.com/samply/beam/">Beam</a>), created by the
 			<a href="https://www.dkfz.de/en/verbis/">German Cancer Research Center (DKFZ)</a>.
 		</p>
 	</div>
 </main>
-
-<footer>
-	<div class="logo">
-		<a href="https://dashboard.eucaim.cancerimage.eu/">
-			<img src="../assets/images/logo_EUCAIM_footer@2x-8.png" alt="" />
-		</a>
-	</div>
-	<div class="links">
-		<a href="https://dashboard.eucaim.cancerimage.eu/privacy-policy">PRIVACY POLICY</a>
-		<a href="http://localhost:4200/#">COOKIES POLICY</a>
-	</div>
-</footer>
 
 <!-- here it waits on all promises to resolve and fills in the parameters -->
 {#await jsonPromises}
